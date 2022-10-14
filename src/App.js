a65d0f3a9a2ab3c50b802e6f9f939d5f7e62e78e
Victor <contactovictore@gmail.com>
2022-10-14 18:08:01 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCelo } from '@celo/react-celo';
+
 
 function App() {
+  const { connect, address } = useCelo();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        {address ? (
+        <div>Connected to {address}</div>
+      ) : (
+        <button onClick={connect}>Connect wallet</button>
+      )}
+
+      
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to relsoad.
         </p>
         <a
           className="App-link"
